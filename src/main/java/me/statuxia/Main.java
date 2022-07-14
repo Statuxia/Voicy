0e0ba5e20c8c9b57689a4a850a790df2369feacf
Statuxia <statuxia@ya.ru>
2022-07-14 21:50:43 +0300

    update Main.java

--- src/main/java/me/statuxia/Main.java
+++ src/main/java/me/statuxia/Main.java
@@ -1,7 +1,31 @@
 package me.statuxia;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import me.statuxia.commands.Admin;
+import me.statuxia.utils.Config;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+
+public class Main extends ListenerAdapter {
+
+    public static JDA jda;
+
+    public static User bot;
+
+    public static void main(String[] args)
+            throws LoginException {
+        jda = JDABuilder.createDefault(Config.TOKEN)
+                .setActivity(Activity.watching(">help"))
+                .setStatus(OnlineStatus.ONLINE)
+                .addEventListeners(new Admin())
+                .build();
+        bot = jda.getSelfUser();
     }
+
 }
