f9756b5a1201d2e1399c29b0690034f3163bcdc4
Statuxia <statuxia@ya.ru>
2022-07-14 21:54:50 +0300

    Just TODO hat

---
+++ src/main/java/me/statuxia/commands/Everyone.java
@@ -0,0 +1,30 @@
+package me.statuxia.commands;
+
+public class Everyone {
+    // TO DO
+    //  Make this comamnds:
+    //   >NOT VOICE
+    //   >help - help command.
+    //   >intite - invite url.
+    //   >ping - pong.
+    //  DEFAULT
+    //   >voice name {name} - edits voice name.
+    //   >voice registered - edits param of age-registered*.
+    //   >voice bitrate {8 to 96. default -> 64} - sets bitrate settings.
+    //   >voice video - sets camera video quallity for all users in channel*.
+    //   >voice limit {0 to 99} - sets limit on voice.
+    //   >voice region {region} - sends list of regions if no choose or error. sets region on voice.
+    //  PERSONAL SETTINGS
+    //   >voice reset - reset all settings.
+    //   >voice clear - clear all allows and denys.
+    //   >voice private - make channel visible only for you*.
+    //   >voice lock - make channel visible for you and allowed users.
+    //   >voice unlock - make channel visible for you and allowed users.
+    //   >voice allow {@user/id} - allows the selected user to join.
+    //   >voice deny {@user/id} - prohibits the selected user from joining.
+    //   >voice invite {@user/id} - sends a message to the user if he is on the server with the bot
+    //   >voice get - transfers rights to the channel if there is no owner.
+    //   >voice update - updates your voice by settings.
+    //   >voice clone {voice} - clone voice settings.
+    //  * -> send again for off.
+}
