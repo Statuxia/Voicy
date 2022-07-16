133893ab77165b09282283278e3cbd9c72fcb939
Statuxia <60938251+Statuxia@users.noreply.github.com>
2022-07-16 18:02:59 +0300

    Config example

---
+++ src/main/java/me/statuxia/utils/Config.java
@@ -0,0 +1,14 @@
+package me.statuxia.utils;
+
+
+public class Config {
+    public static final String TOKEN = "BOT_TOKEN"; // PUT BOT TOKEN HERE
+
+    public static final String PREFIX = ">";
+
+    public static final String PREFIX_VOICE = ">voice";
+
+    public static final String DATABASE_HOST = "jdbc:mysql://localhost:3306/voicy";
+    public static final String DATABASE_USER = "root";
+    public static final String DATABASE_PASS = "root";
+}
